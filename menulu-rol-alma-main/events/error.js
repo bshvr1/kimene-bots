d19adc396a43ab0c4aca5413007458767c419528
Can <77938499+Canzade@users.noreply.github.com>
2021-12-06 23:34:30 +0300

    Delete menulu-rol-alma-main directory

--- menulu-rol-alma-main/events/error.js
+++
@@ -1,9 +0,0 @@
-module.exports = class {
-  constructor (client) {
-    this.client = client;
-  }
-
-  async run (error) {
-    this.client.logger.log(`Discord.js tarafından bir hata oluştu: \n${JSON.stringify(error)}`, "error");
-  }
-};
